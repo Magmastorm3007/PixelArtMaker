2341c8b595a7cb9bfd2cc9d30a69277f925263ca
Magmastorm <shashwat.nath@hotmail.com>
2022-06-28 18:27:55 +0530

    gh-pages deploy

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+
+import '../styles/App.scss';
+import Editor from '../components/Editor'
+function App() {
+  return (
+    <div className="App">
+      
+         <Editor/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DrawingPanel.js
@@ -0,0 +1,30 @@
+import React, { useRef } from "react";
+import "../styles/drawingPanel.scss";
+import Row from "./Row";
+import { exportComponentAsJPEG, exportComponentAsPNG } from "react-component-export-image";
+
+export default function DrawingPanel(props) {
+  const { width, height, selectedColor } = props;
+
+  const panelRef = useRef();
+
+  let rows = [];
+
+  for (let i = 0; i < height; i++) {
+    rows.push(<Row key={i} width={width} selectedColor={selectedColor} />);
+  }
+
+  return (
+    <div id="drawingPanel">
+      <div id="pixels" ref={panelRef}>
+        {rows}
+      </div>
+      <button onClick={() => exportComponentAsJPEG(panelRef)} className="button">
+      Export as JPEG
+      </button>
+      <button onClick={() => exportComponentAsPNG(panelRef)} className="button">
+      Export as PNG
+      </button>
+    </div>
+  );
+}
